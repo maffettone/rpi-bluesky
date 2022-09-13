7f46b01928e4abaf68afc7bd3a46fbab55ce1af2
maffettone <phillip.maffettone@gmail.com>
2022-09-13 19:50:24 -0400

    RGB with example random walk

--- rpi_bluesky/ophyd/devices.py
+++ rpi_bluesky/ophyd/devices.py
@@ -1,6 +1,14 @@
-from rpi_bluesky.ophyd import RpiSignal, RpiPWM, RpiComponent, RpiDevice
+from rpi_bluesky.ophyd import RpiComponent, RpiDevice, RpiPWM, RpiSignal
 
 
 class LED(RpiDevice):
     io = RpiComponent(RpiSignal, name="io")
     pwm = RpiComponent(RpiPWM, name="pwm")
+
+
+class RGB_LED(RpiDevice):
+    """Notice how this device requires hard coding of the channels"""
+
+    red = RpiComponent(LED, pin=11, name="red")
+    green = RpiComponent(LED, pin=13, name="green")
+    blue = RpiComponent(LED, pin=15, name="blue")
